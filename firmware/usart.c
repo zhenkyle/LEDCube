eda1ef8e8c92ca344115b3ea286844a3e9facc90
Zhenkyle <zhenkyle@gmail.com>
2018-11-27 21:18:26 +0800

    change USART bit

--- firmware/usart.c
+++ firmware/usart.c
@@ -19,12 +19,12 @@ void initUSART(void) {                                /* requires BAUD */
   UCSR0B = (1 << TXEN0) | (1 << RXEN0);
  
   // UCSRC - USART control register
-  // bit 7-6      sync/ascyn 00 = async,  01 = sync
+  // bit 7-6      sync/ascyn 00 = async,  01 = sync ( 00 = async)
   // bit 5-4      parity 00 = disabled
-  // bit 3        stop bits 0 = 1 bit  1 = 2 bits
+  // bit 3        stop bits 0 = 1 bit  1 = 2 bits ( 0 = 1 bit)
   // bit 2-1      frame length 11 = 8
   // bit 0        clock polarity = 0
-  UCSR0C  = 0b10000110;
+  UCSR0C  = 0b00000110;
  
   // Enable uart, tx and rx
   UDR0 = 0x00; // send an empty byte to indicate powerup.
